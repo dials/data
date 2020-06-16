efe0a375ecc544a26d24cec1543cfc0a9affed8d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-06-17 00:12:56 +0100

    setuptools is no longer required - I believe

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["importlib_resources>=1.1", "pytest", "pyyaml", "setuptools"]
+requirements = ["importlib_resources>=1.1", "pytest", "pyyaml"]
 setup_requirements = []
 test_requirements = []
 
