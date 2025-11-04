cf38d4c5f5146c81f1bec8af61b7462f09b17ed3
David Waterman <dgwaterman@gmail.com>
2025-11-04 19:58:54 +0000

    attempt to fix tests

--- tests/test_dials_data.py
+++ tests/test_dials_data.py
@@ -22,14 +22,14 @@ def test_repository_location():
 
 def test_fetching_undefined_datasets_does_not_crash():
     df = dials_data.download.DataFetcher(read_only=True)
-    assert df("aardvark", pathlib=True) is False
+    assert df("aardvark") is False
 
 
 def test_requests_for_future_datasets_can_be_intercepted():
     df = dials_data.download.DataFetcher(read_only=True)
     df.result_filter = mock.Mock()
     df.result_filter.return_value = False
-    assert df("aardvark", pathlib=True) is False
+    assert df("aardvark") is False
     df.result_filter.assert_called_once_with(result=False)
 
 
@@ -48,34 +48,7 @@ def test_datafetcher_constructs_py_path(fetcher, root):
         "dataset", pre_scan=True, read_only=False, verify=True
     )
 
-    ds = df("dataset", pathlib=False)
+    ds = df("dataset")
     assert pathlib.Path(ds).resolve() == pathlib.Path("/tmp/root/dataset").resolve()
     assert isinstance(ds, py.path.local)
     fetcher.assert_called_once()
-
-
-@mock.patch("dials_data.datasets.repository_location")
-@mock.patch("dials_data.download.fetch_dataset")
-def test_datafetcher_constructs_path(fetcher, root):
-    test_path = pathlib.Path("/tmp/root")
-    root.return_value = test_path
-    fetcher.return_value = True
-
-    df = dials_data.download.DataFetcher(read_only=True)
-    ds = df("dataset", pathlib=True)
-    assert ds == test_path / "dataset"
-
-    assert isinstance(ds, pathlib.Path)
-    fetcher.assert_called_once_with(
-        "dataset", pre_scan=True, read_only=False, verify=True
-    )
-
-    with pytest.warns(DeprecationWarning):
-        ds = df("dataset")
-    assert pathlib.Path(ds).resolve() == test_path.joinpath("dataset").resolve()
-    assert not isinstance(
-        ds, pathlib.Path
-    )  # default is currently to return py.path.local()
-    fetcher.assert_called_once_with(
-        "dataset", pre_scan=True, read_only=False, verify=True
-    )
